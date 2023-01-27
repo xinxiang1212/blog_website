b449424cded1e814fb0aa2e0ec282fcc6fc19832
xinxiang <xxiang121@gmail.com>
2023-01-27 13:54:36 -0800

    chapter 3 finished

--- hello.py
+++ hello.py
@@ -1,14 +1,18 @@
+from datetime import datetime
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
 
+moment = Moment(app)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
