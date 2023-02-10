44cde63f34aca90557ded248e1d97fd264f13678
xinxiang <xxiang121@gmail.com>
2023-02-10 10:37:23 -0800

    4a

--- hello.py
+++ hello.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtfomrs import StringField, SubmitField
-from wtforms import DataRequired
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
@@ -12,30 +12,20 @@ bootstrap = Bootstrap(app)
 
 moment = Moment(app)
 
+app.config['SECRET_KEY'] = 'hard to guess string'
+
 
 class NameForm(FlaskForm):
     name = StringField('what is your name', validators=[DataRequired()])
     submit = SubmitField('submit')
 
-
-app.config['SECRET_KEY'] = 'hard to guess string'
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
 
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template(500), 500
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=name)
 
