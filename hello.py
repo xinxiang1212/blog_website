01dd75e55afd02a9e032caa3b877dcc1a7c5c3fe
xinxiang <xxiang121@gmail.com>
2023-02-25 13:59:35 -0800

    5a

--- hello.py
+++ hello.py
@@ -4,29 +4,29 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-from flask_sqlalchemy import SQLALCHEMY
+from flask_sqlalchemy import SQLAlchemy
 
-basedir = os.path.abspath(os.path.dirname(_file_))
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] =\
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLALCHEMY(app)
+db = SQLAlchemy(app)
 
-class Role(db.model)
-    _tablename_ = "roles"
+class Role(db.Model):
+    __tablename__ = "roles"
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(64), unique = True)
-    users = db.relationship('User', backref='role')
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
     def _repr_(self):
         return '<Role %r>' % self.name
 
 
-class User(db.model)
-    _tablename_ = "users"
+class User(db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index = True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
