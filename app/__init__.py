bf8615b5f127e3895e603af6960e59b86a497053
xinxiang <xxiang121@gmail.com>
2023-03-08 10:10:35 -0800

    user login and logout

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,10 @@ from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import config
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 mail = Mail()
@@ -19,11 +23,12 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     from .auth import auth as auth_blueprint
-    app.register_blueprint(url_prefix='/auth')
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
