5ef22cb3fcc8ba812ef2d3f546a895e7c95f7be0
xinxiang <xxiang121@gmail.com>
2023-04-26 10:36:39 -0700

    chapter 11 blog post: blog post submission and display(11a)

--- flask1.py
+++ flask1.py
@@ -2,7 +2,7 @@ import os
 import click
 from flask_migrate import Migrate
 from app import create_app, db
-from app.models import User, Role, Permission
+from app.models import User, Role, Post, Permission
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
